b50ad982834d2e9fef5ae76ae8243f239fc6924c
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-11-09 20:26:53 +0200

    add day 6 solutions

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use crate::util;
 
 pub fn solve(level: &util::Level) -> i64 {
@@ -7,18 +9,102 @@ pub fn solve(level: &util::Level) -> i64 {
     }
 }
 
-pub fn part1() -> i64 {
-    let _input = util::load_input(6);
+struct Rectangle {
+    left: usize,
+    right: usize,
+    top: usize,
+    bottom: usize,
+}
 
-    let answer = 0;
+enum OpType {
+    TurnOn,
+    TurnOff,
+    Toggle,
+}
+struct Operation {
+    op_type: OpType,
+    rect: Rectangle,
+}
+
+impl Operation {
+    fn parse(input: &str) -> Self {
+        let op_type;
+        if Regex::new(r"turn on").unwrap().is_match(input) {
+            op_type = OpType::TurnOn
+        } else if Regex::new(r"turn of").unwrap().is_match(input) {
+            op_type = OpType::TurnOff
+        } else {
+            op_type = OpType::Toggle
+        };
+
+        let rect_re = Regex::new(r"(\d+),(\d+) through (\d+),(\d+)").unwrap();
+        let (_, [left, top, right, bottom]) = rect_re.captures(input).unwrap().extract();
+
+        Self {
+            op_type,
+            rect: Rectangle {
+                left: left.parse().unwrap(),
+                right: right.parse().unwrap(),
+                top: top.parse().unwrap(),
+                bottom: bottom.parse().unwrap(),
+            },
+        }
+    }
+}
+
+pub fn part1() -> i64 {
+    let input = util::load_input(6);
+
+    let mut grid = vec![vec![false; 1000]; 1000];
+
+    for line in input.lines() {
+        let Operation { op_type, rect } = Operation::parse(line);
+        for i in rect.left..=rect.right {
+            for j in rect.top..=rect.bottom {
+                grid[i][j] = match op_type {
+                    OpType::TurnOn => true,
+                    OpType::TurnOff => false,
+                    OpType::Toggle => !grid[i][j],
+                }
+            }
+        }
+    }
+
+    let answer = grid.iter().fold(0, |sum, row| {
+        sum + row
+            .iter()
+            .map(|a| match a {
+                true => 1,
+                false => 0,
+            })
+            .sum::<i64>()
+    });
 
     answer
 }
 
 pub fn part2() -> i64 {
-    let _input = util::load_input(6);
+    let input = util::load_input(6);
 
-    let answer = 0;
+    let mut grid = vec![vec![0_i64; 1000]; 1000];
+
+    for line in input.lines() {
+        let Operation { op_type, rect } = Operation::parse(line);
+        for i in rect.left..=rect.right {
+            for j in rect.top..=rect.bottom {
+                grid[i][j] += match op_type {
+                    OpType::TurnOn => 1,
+                    OpType::TurnOff => {match grid[i][j] > 0 {
+                        true => -1,
+                        false => 0,
+                    }},
+                    OpType::Toggle => 2,
+                }
+            }
+        }
+    }
+
+    let answer = grid.iter().map(|row| row.iter().sum::<i64>()).sum();
 
     answer
 }
