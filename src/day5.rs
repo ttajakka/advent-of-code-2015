3a16beed2a9b92f395a2efa0cc52df80c7be9c9e
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2025-11-09 13:28:18 +0200

    add day 5 level 1 solution

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,5 @@
 use crate::util;
+use regex::Regex;
 
 pub fn solve(level: &util::Level) -> i64 {
     match level {
@@ -8,9 +9,16 @@ pub fn solve(level: &util::Level) -> i64 {
 }
 
 pub fn part1() -> i64 {
-    let _input = util::load_input(5);
+    let input = util::load_input(5);
 
-    let answer = 0;
+    let mut answer = 0;
+
+    for line in input.lines() {
+        if is_nice(line) {
+            println!("{line}");
+            answer += 1;
+        }
+    }
 
     answer
 }
@@ -21,4 +29,27 @@ pub fn part2() -> i64 {
     let answer = 0;
 
     answer
+}
+
+fn is_nice(line: &str) -> bool {
+    let disallowed = Regex::new(r"ab|cd|pq|xy").unwrap();
+    if disallowed.is_match(line) {
+        return false;
+    }
+
+    let vowels = Regex::new(r"((a|e|i|o|u).*){3,}").unwrap();
+    if !vowels.is_match(line) {
+        return false;
+    }
+
+    let mut chars = line.chars();
+    let mut previous = chars.next().unwrap();
+    for c in chars {
+        if c == previous {
+            return true;
+        }
+        previous = c;
+    }
+
+    return false
 }
