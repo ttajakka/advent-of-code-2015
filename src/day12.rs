3509139b8ccae60db4e292a9611734db1cdc4adc
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2026-04-22 17:14:46 +0300

    add say 12 part 1 solution

--- src/day12.rs
+++ src/day12.rs
@@ -8,11 +8,19 @@ pub fn solve(level: &util::Level) -> String {
 }
 
 pub fn part1() -> String {
-    let _input = util::load_input(12);
+    let input = util::load_input(12);
 
-    let answer = String::new();
+    let mut answer = 0;
+    for chunk in input.split(&[':', ',', '"', '{', '}', '[', ']']) {
+            if let Ok(num) = chunk.parse::<i64>() {
+                answer += num
+            } else if chunk.contains(&['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']) {
+                println!("{chunk}")
+            }
+    }
 
-    answer
+
+    answer.to_string()
 }
 
 pub fn part2() -> String {
