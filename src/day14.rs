e62fb76e1c2515cae2cd5c24a5b16ed5367027ba
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2026-04-23 15:19:45 +0300

    add day 14 part 2 solution

--- src/day14.rs
+++ src/day14.rs
@@ -9,13 +9,51 @@ pub fn solve(level: &util::Level) -> String {
 
 const TOTAL_TIME: u32 = 2503;
 
-struct Reindeer {
+#[derive(Debug)]
+struct ReindeerProps {
     speed: u32,
     movement_duration: u32,
     rest_duration: u32,
 }
 
+#[derive(Debug)]
+enum MovementState {
+    Moved(u32),
+    Rested(u32),
+}
+
+#[derive(Debug)]
+struct Reindeer {
+    props: ReindeerProps,
+    state: MovementState,
+    travelled: u32,
+    score: u32,
+}
+
 impl Reindeer {
+    fn progress(&mut self) {
+        match self.state {
+            MovementState::Moved(t) => {
+                if t == self.props.movement_duration {
+                    self.state = MovementState::Rested(1);
+                } else {
+                    self.state = MovementState::Moved(t + 1);
+                    self.travelled += self.props.speed;
+                }
+            }
+            MovementState::Rested(t) => {
+                if t == self.props.rest_duration {
+                    self.state = MovementState::Moved(1);
+                    self.travelled += self.props.speed;
+                } else {
+                    self.state = MovementState::Rested(t + 1);
+                }
+            }
+        }
+    }
+}
+
+impl ReindeerProps {
     fn calculate_travelled_distance(&self, total_time: u32) -> u32 {
         let cycle_length = self.movement_duration + self.rest_duration;
         let movement_rest_cycles = total_time / cycle_length;
@@ -25,19 +63,19 @@ impl Reindeer {
     }
 }
 
-fn parse_input(input: String) -> Vec<Reindeer> {
-    let mut reindeers = vec![];
+fn parse_input(input: String) -> Vec<ReindeerProps> {
+    let mut props = vec![];
 
     for line in input.lines() {
         let parts: Vec<_> = line.split_whitespace().collect();
-        reindeers.push(Reindeer {
+        props.push(ReindeerProps {
             speed: parts[3].parse().expect("speed should be 3rd"),
             movement_duration: parts[6].parse().expect("movement duration should be 6th"),
             rest_duration: parts[13].parse().expect("rest duration should be 13th"),
         })
     }
 
-    reindeers
+    props
 }
 
 pub fn part1() -> String {
@@ -59,9 +97,33 @@ pub fn part1() -> String {
 }
 
 pub fn part2() -> String {
-    let _input = util::load_input(14);
+    let input = util::load_input(14);
 
-    let answer = String::new();
+    let props = parse_input(input);
+
+    let mut reindeers: Vec<_> = props
+        .into_iter()
+        .map(|props| Reindeer {
+            props,
+            state: MovementState::Moved(0),
+            travelled: 0,
+            score: 0,
+        })
+        .collect();
+
+    for _ in 0..TOTAL_TIME {
+        reindeers.iter_mut().for_each(|r| r.progress());
+        let max_distance = reindeers.iter().map(|r| r.travelled).max().unwrap();
+        reindeers.iter_mut().for_each(|r| {
+            if r.travelled == max_distance {
+                r.score += 1;
+            }
+        });
+    }
+
+    let answer = reindeers.iter().map(|r| r.score).max().unwrap();
+
+    let answer = answer.to_string();
 
     answer
 }
