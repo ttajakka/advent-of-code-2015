48e42d84358ca2fdcdab8a078b4efe1d378e61da
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2026-04-24 00:29:25 +0300

    add day 15 solutions

--- src/day15.rs
+++ src/day15.rs
@@ -7,18 +7,97 @@ pub fn solve(level: &util::Level) -> String {
     }
 }
 
+fn parse_input(input: String) -> Vec<Vec<i32>> {
+    input
+        .replace(',', "")
+        .lines()
+        .map(|l| {
+            let parts: Vec<_> = l.split_whitespace().collect();
+            vec![2_usize, 4, 6, 8, 10]
+                .into_iter()
+                .map(|i| parts[i].parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
 pub fn part1() -> String {
-    let _input = util::load_input(15);
+    let input = util::load_input(15);
 
-    let answer = String::new();
+    let columns = parse_input(input);
+    let mut answer = 0;
+
+    // loop over all subsets of size 4 in a set the set {1,...,100}
+    for i in 0..103 {
+        for j in i..103 {
+            for k in j..103 {
+                let weights = vec![i, j - i - 1, k - j - 1, 103 - k - 1];
+
+                let score: i32 = (0_usize..4)
+                    .map(|x| {
+                        columns
+                            .iter()
+                            .enumerate()
+                            .map(|(s, c)| weights[s] * c[x])
+                            .sum::<i32>()
+                            .max(0)
+                    })
+                    .product();
+
+                if score > answer {
+                    answer = score;
+                }
+            }
+        }
+    }
+
+    let answer = answer.to_string();
 
     answer
 }
 
 pub fn part2() -> String {
-    let _input = util::load_input(15);
+    let input = util::load_input(15);
 
-    let answer = String::new();
+    let columns = parse_input(input);
+
+    let mut answer = 0;
+
+    // loop over all subsets of size 4 in a set the set {1,...,100}
+    for i in 0..103 {
+        for j in i..103 {
+            for k in j..103 {
+                let weights = vec![i, j - i - 1, k - j - 1, 103 - k - 1];
+
+                let calories: i32 = columns
+                    .iter()
+                    .enumerate()
+                    .map(|(s, c)| weights[s] * c[4])
+                    .sum();
+                let calories = calories.max(0);
+
+                if calories != 500 {
+                    continue;
+                }
+
+                let score: i32 = (0_usize..4)
+                    .map(|x| {
+                        columns
+                            .iter()
+                            .enumerate()
+                            .map(|(s, c)| weights[s] * c[x])
+                            .sum::<i32>()
+                            .max(0)
+                    })
+                    .product();
+
+                if score > answer {
+                    answer = score;
+                }
+            }
+        }
+    }
+
+    let answer = answer.to_string();
 
     answer
 }
