81db1d16609b2ee8813e861621ee5b315e5c0090
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2026-05-03 11:59:03 +0300

    add day 19 part 1 solution

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::util;
 
 pub fn solve(level: &util::Level) -> String {
@@ -8,11 +10,67 @@ pub fn solve(level: &util::Level) -> String {
 }
 
 pub fn part1() -> String {
-    let _input = util::load_input(19);
+    let input = util::load_input(19);
 
-    let answer = String::new();
+    let (replacements, original) = parse_input(input);
+
+    let mut seen = HashSet::<String>::new();
+
+    for (i, atom) in original.iter().enumerate() {
+        if let Some(repls) = replacements.get(atom) {
+            for r in repls {
+                let (head, tail) = original.split_at(i + 1);
+                let (head, _removed) = head.split_at(i);
+
+                let mut head = head.to_owned();
+                let mut r_owned = r.to_owned();
+                let mut tail = tail.to_owned();
+
+                head.append(&mut r_owned);
+                head.append(&mut tail);
+
+                seen.insert(head.join(""));
+            }
+        }
+    }
+
+    let answer = seen.len().to_string();
 
     answer
+}
+
+fn parse_input(input: String) -> (HashMap<String, Vec<Vec<String>>>, Vec<String>) {
+    let mut replacements = HashMap::new();
+
+    let mut parts = input.split("\n\n");
+    let replacements_str = parts.next().unwrap();
+
+    for l in replacements_str.lines() {
+        let mut parts = l.split(" => ");
+        let key = parts.next().unwrap().to_string();
+        let values = split_molecule(parts.next().unwrap().to_string());
+
+        replacements.entry(key).or_insert(vec![]).push(values);
+    }
+
+    let molecule = split_molecule(parts.next().unwrap().lines().next().unwrap().to_string());
+
+    (replacements, molecule)
+}
+
+fn split_molecule(molecule_str: String) -> Vec<String> {
+    let mut molecule_str = molecule_str;
+    let mut molecule = vec![];
+
+    while let Some(index) = molecule_str.rfind(char::is_uppercase) {
+        let (new, atom) = molecule_str.split_at(index);
+        molecule.push(atom.to_string());
+        molecule_str = new.to_string();
+    }
+
+    molecule.reverse();
+
+    molecule
 }
 
 pub fn part2() -> String {
