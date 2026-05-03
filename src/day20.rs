6b4ba311210104cca2efa519e4d374a14ebfe108
Tuomas Tajakka <tuomas.tajakka@gmail.com>
2026-05-03 14:03:33 +0300

    add day 20 part 1 solution

--- src/day20.rs
+++ src/day20.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::util;
 
 pub fn solve(level: &util::Level) -> String {
@@ -8,11 +10,60 @@ pub fn solve(level: &util::Level) -> String {
 }
 
 pub fn part1() -> String {
-    let _input = util::load_input(20);
+    let input = util::load_input(20);
 
-    let answer = String::new();
+    let input: u64 = input.lines().next().unwrap().parse::<u64>().unwrap();
+
+    let mut answer = 0;
+    let mut largest = 0;
+
+    for i in 2.. { // sum_of_factors doesn't work for input = 1
+        let presents = sum_of_factors(i) * 10;
+        if presents >= input {
+            answer = i;
+            break;
+        } else if presents > largest {
+            largest = presents;
+        }
+    }
+
+    let answer = answer.to_string();
 
     answer
+}
+
+fn sum_of_factors(input: u64) -> u64 {
+    let factors_with_multiplicities = factor_with_multiplicities(input);
+
+    factors_with_multiplicities
+        .iter()
+        .map(|(p, e)| (p.pow(*e + 1) - 1) / (p - 1))
+        .product()
+}
+
+fn factor_with_multiplicities(input: u64) -> Vec<(u64, u32)> {
+    let mut factors: HashMap<u64, u32> = HashMap::new();
+    let mut input = input;
+    'outer: loop {
+        let mut cand = 2;
+        while cand * cand <= input {
+            if input % cand == 0 {
+                input = input / cand;
+                let entry = factors.entry(cand).or_insert(0);
+                *entry += 1;
+
+                continue 'outer;
+            }
+
+            cand += 1;
+        }
+
+        break;
+    }
+
+    *factors.entry(input).or_insert(0) += 1;
+
+    factors.into_iter().collect()
 }
 
 pub fn part2() -> String {
